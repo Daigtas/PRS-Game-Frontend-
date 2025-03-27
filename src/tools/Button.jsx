099fc714bd66f4bac068cc8086dfbf5da99b065d
Daigtas <lukminas.a@gmail.com>
2025-03-27 20:26:36 +0200

    Created Button tool to use in project

---
+++ src/tools/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Button({ type = 'secondary', onClick, children, className = '', ...props }) {
+  return (
+    <button 
+      className={`btn ${type === 'primary' ? 'primary-btn' : 'secondary-btn'} ${className}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
+//usage <Button type="primary" onClick={onPlay} className="css-class-name" >
+//types: primary, secondary
+//onClick = js function
