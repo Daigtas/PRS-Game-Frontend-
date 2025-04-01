30280651abcb61044f8e9bc72fd921610ab2ff0a
Daigtas <lukminas.a@gmail.com>
2025-04-01 20:45:40 +0300

    pafixinta :)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Login from './Auth/Login';
-import Register from './Auth/Register';
 import Reset from './Reset';
 import Rules from './Rules/Rules';
 
@@ -9,8 +7,6 @@ function Footer({ resetGame, userState, setUserState }) {
     <div className="buttons">
       <Reset resetGame={resetGame} />
       <Rules />
-      <Login userState={userState} setUserState={setUserState} />
-      <Register userState={userState} setUserState={setUserState} />
     </div>
   );
 }
