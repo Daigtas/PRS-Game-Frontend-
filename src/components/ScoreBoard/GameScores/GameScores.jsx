f4c1240d635334e7323cef83c95f1d63747c334f
Giedrius Mizaras <giedrius.mizaras@gmail.com>
2025-04-03 21:35:46 +0300

    GameScore update

--- src/components/ScoreBoard/GameScores/GameScores.jsx
+++ src/components/ScoreBoard/GameScores/GameScores.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./GameScores.css"
 import TitleBar from "./TitleBar";
+import ScoreBox from "./ScoreBox";
+import Button from "../../../tools/Button";
 
 const GameScores = () => {
     return(
         <div className="game-scores-wrapper">
             <TitleBar/>
+            <div className="scores">
+                <ScoreBox title = "Player"/>
+                <ScoreBox title = "PC"/>
+            </div>
+            <Button>HighScores</Button>
+            
         </div>
     )
 }
