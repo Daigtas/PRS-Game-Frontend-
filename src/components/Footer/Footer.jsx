9d805b2b3758453f45f0e5ec69a85641ea47e382
Daigtas <lukminas.a@gmail.com>
2025-04-03 21:45:49 +0300

    Made a playboard and lots of game logic.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Reset from './Reset';
 import Rules from './Rules/Rules';
+import Button from '../../tools/Button';
 
 function Footer({ resetGame, userState, setUserState }) {
   return (
     <div className="buttons">
       <Reset resetGame={resetGame} />
       <Rules />
+      <Button>Login</Button>
+      <Button>Register</Button>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,22 @@
-import React, { Children } from "react";
+import React from "react";
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
 import PlayBoard from './PlayBoard/PlayBoard'
 import ScoreBoard from './ScoreBoard/ScoreBoard'
-const Main = () => {
+
+const Main = () => { gameState, userState, updatePlayerChoice, playGame, resetGame, setUserState
 return (
     <div className="container">
         <Header />
         <ScoreBoard />
-        <PlayBoard />
-        <Footer 
-          resetGame={resetGame}
-          userState={userState}
-          setUserState={setUserState}
+        <PlayBoard 
+          playerChoice={gameState.playerChoice}
+          computerChoice={gameState.computerChoice}
+          result={gameState.result}
+          updatePlayerChoice={updatePlayerChoice}
+          playGame={playGame}
         />
+        <Footer />
     </div>
 )
 };
